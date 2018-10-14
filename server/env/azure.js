aebe08ed18384efce2167a024ae90b59ef3aa4a1
Tri Dang <tri.dang@innitico.com>
2018-10-13 22:48:30 -0400

    remove azure related information from code

--- server/env/azure.js
+++ server/env/azure.js
@@ -1,10 +1,10 @@
-const cosmosPort = 10255; // replace with your port
-const dbName = 'books';
-const key = 'MPYByg3ZaGSqrc7u6gBMcj0TUZgWLAXUG5hKOqFc3knNeiLa6ERI9gpv7JH0cHYDPhKPzTrZEnfHZOp6LRVLvw%3D%3D';
+const cosmosPort = Number(process.env.COSMODB_PORT); // replace with your port
+const dbName = process.env.COSMODB_NAME;
+const key = process.env.COSMODB_KEY;
 const mongodb_url = `mongodb://${dbName}:${key}@${dbName}.documents.azure.com:${cosmosPort}/?ssl=true`;
 const mongo_options = { useNewUrlParser: true };
-const eventHubConnectionString='Endpoint=sb://mean-world.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=v4XsVaC/xe/r63oKmPaYMu4ffvelYZ+BvU4AAR0nJQw='
-const eventHubentityPath='mean-world-app'
+const eventHubConnectionString=process.env.EVENT_HUB_CONNECTION_STRING
+const eventHubentityPath=process.env.EVENT_HUB_ENTITY_NAME
 
 module.exports = {  
   mongodb_url,
